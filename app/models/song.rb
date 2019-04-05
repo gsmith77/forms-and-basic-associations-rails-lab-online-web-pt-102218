96cbdafdfb07b6d5fa68cb97c0b8681976fac605
github username <github email address>
2019-04-04 22:10:47 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if content.length != 0
+        self.notes << Note.find_or_create_by(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes ? self.notes : nil
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :artists
-  resources :songs
+  resources :artists, :songs, only: [:new, :index, :show, :create, :edit, :update, :destroy]
 end
